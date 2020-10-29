54ae92dee7df79e8e80cb41d93618a8dd9dbf4e8
sam202020 <shalomlandsman@gmail.com>
2020-10-28 21:41:25 -0500

    clean up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  SANDBOX_APP_ID: 'sandbox-sq0idb-oIuWkzoUJ8mEoRWbd1N2BA',
+  SANDBOX_TOKEN:
+    'EAAAEGzB5TTlyZDdz3M45E-cYAW6FGuLyDN-VOUKcNzY57TIgvxGn2kqBlN4mu6-',
+  SANDBOX_LOCATION: 'LCH842Y98K67A',
 };
