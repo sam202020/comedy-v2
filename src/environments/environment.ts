c880048f5af252e0a079404eff132fe53bf3c0f2
sam202020 <shalomlandsman@gmail.com>
2020-11-12 14:12:24 -0600

    fix location

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   SANDBOX_APP_ID: 'sandbox-sq0idb-oIuWkzoUJ8mEoRWbd1N2BA',
-  SANDBOX_LOCATION: 'LCH842Y98K67A',
+  SANDBOX_LOCATION: 'RHD0C0D8H3ND2',
   PRODUCTION_APP_ID: 'sq0idp-MCf3zznRyuvZt-L-u5N2aA',
   PRODUCTION_LOCATION: 'RHD0C0D8H3ND2',
 };
